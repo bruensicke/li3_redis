7ad3d0d5e04f48f86d0268b147637aca1a9d3893
d1rk <d1rk@gmx.de>
2012-10-22 15:44:04 +0200

    first draft of a working redis datasource

---
+++ extensions/data/source/Redis.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * li3_redis: A Redis datasource for Lithium.
+ *
+ * @copyright     Copyright 2012, Dirk Brünsicke
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_redis\extensions\data\source;
+
+use Exception;
+use Redis as RedisCore;
+use lithium\core\NetworkException;
+
+/**
+ * A data source adapter which allows you to connect to the Redis database engine.
+ */
+class Redis extends \lithium\data\Source {
+
+	/**
+	 *
+	 */
+	public function __construct(array $config = array()) {
+		$defaults = array(
+			'host'       => 'localhost',
+			'port'       => '6379',
+			'timeout'    => '3',
+			'password'   => null,
+			'database'   => null,
+			'persistent' => false
+		);
+		parent::__construct($config + $defaults);
+	}
+
+	/**
+	 * With no parameter, checks to see if the `redis` extension is installed. With a
+	 * parameter, queries for a specific supported feature.
+	 *
+	 * @param string $feature Test for support for a specific feature, i.e. `"arrays"`.
+	 * @return boolean Returns `true` if the particular feature support is enabled (or `false`).
+	 */
+	public static function enabled($feature = null) {
+		if (!$feature) {
+			return extension_loaded('redis');
+		}
+		$features = array(
+			'arrays' => true,
+			'transactions' => true,
+			'booleans' => true,
+			'relationships' => false
+		);
+		return isset($features[$feature]) ? $features[$feature] : null;
+	}
+
+	/**
+	 *
+	 */
+	public function connect() {
+		$config = $this->_config;
+		$this->_isConnected = false;
+
+		if (stristr(':', $config['host'])) {
+			list($host, $port) = explode(':', $config['host']);
+		} else {
+			$host = $config['host'];
+			$port = $config['port'];
+		}
+		$method = $config['persistent'] ? 'pconnect' : 'connect';
+
+		try {
+			$this->connection = new RedisCore();
+			if (!empty($config['password'])) {
+				$this->connection->auth($config['password']);
+			}
+			$this->connection->$method($host, $port, $config['timeout']);
+			if (!empty($config['database'])) {
+				$this->connection->select($config['database']);
+			}
+		} catch(Exception $e) {
+			throw new NetworkException("Could not connect to the database: " . $e->getMessage(), 503);
+			return false;
+		}
+
+		return $this->_isConnected = true;
+	}
+
+	/**
+	 * Dispatches a not-found method to the Redis connection object.
+	 *
+	 * If you want to know, what methods are available, have a look at the readme of phprdis.
+	 * One use-case might be to query possible keys, e.g.
+	 *
+	 * {{{Connections::get('li3_redis')->keys('*');}}}
+	 *
+	 * @link https://github.com/nicolasff/phpredis GitHub: PhpRedis Extension
+	 * @param string $method Name of the method to call
+	 * @param array $params Parameter list to use when calling $method
+	 * @return mixed Returns the result of the method call
+	 */
+	public function __call($method, $params = array()) {
+		return call_user_func_array(array(&$this->connection, $method), $params);
+	}
+
+	/**
+	 *
+	 */
+	public function disconnect() {
+		$this->_isConnected = false;
+		unset($this->connection);
+		return true;
+	}
+
+	/**
+	 *
+	 */
+	public function sources($class = null) {}
+
+	/**
+	 *
+	 */
+	public function describe($entity, array $meta = array()) {}
+
+	/**
+	 *
+	 */
+	public function relationship($class, $type, $name, array $options = array()) {}
+
+	/**
+	 *
+	 */
+	public function create($query, array $options = array()) {}
+
+	/**
+	 *
+	 */
+	public function read($query, array $options = array()) {}
+
+	/**
+	 *
+	 */
+	public function update($query, array $options = array()) {}
+
+	/**
+	 *
+	 */
+	public function delete($query, array $options = array()) {}
+
+}
+
+?>
